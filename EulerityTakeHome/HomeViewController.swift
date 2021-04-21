222f36c458bf2ae3b79c8e46e8eeae0977b6d651
Aaron Cleveland <toscleveland@gmail.com>
2021-04-21 13:13:28 -0500

    Flipped project to programmatic ui

---
+++ EulerityTakeHome/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  EulerityTakeHome
+//
+//  Created by Aaron Cleveland on 4/21/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+}
