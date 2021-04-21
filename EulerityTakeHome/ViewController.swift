b5ea5cb742ddc9ee575b5b998016bb86c6498283
Aaron Cleveland <toscleveland@gmail.com>
2021-04-21 13:00:59 -0500

    Initial Commit

---
+++ EulerityTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EulerityTakeHome
+//
+//  Created by Aaron Cleveland on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
