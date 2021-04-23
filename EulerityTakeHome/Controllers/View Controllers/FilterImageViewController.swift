9dadb9b1ac7ffabc32afe864f7d335b2c25603ab
Aaron Cleveland <toscleveland@gmail.com>
2021-04-23 15:00:38 -0500

    Cleaned up UI, added upload code still WIP.

--- EulerityTakeHome/Controllers/View Controllers/FilterImageViewController.swift
+++ EulerityTakeHome/Controllers/View Controllers/FilterImageViewController.swift
@@ -14,16 +14,22 @@ class FilterImageViewController: UIViewController {
     let sliderSize: CGFloat = 200
     let thumbnailSize: CGFloat = 80
     
-    var aCIImage = CIImage();
+    var ciImage = CIImage();
     var context = CIContext();
     var outputImage = CIImage();
     var newUIImage = UIImage();
+    
+    let networkManager = NetworkManager()
     
     // MARK: - ImageViews -
     lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
         return imageView
     }()
     
@@ -152,21 +158,42 @@ class FilterImageViewController: UIViewController {
         stack.spacing = 20
         return stack
     }()
+    
+    lazy var presetFilterButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Preset Filter", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .systemTeal
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(presetFilterAction), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var filterButtons: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 16
+        return stack
+    }()
 
+    let textView = UITextView(frame: .zero)
+    
     // MARK: - Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        navigationItem.title = "Edit Image"
         constraints()
+        view.backgroundColor = .white
+        navigationItem.title = "Edit Image"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Upload", style: .plain, target: self, action: #selector(saveImage))
         
-        let aUIImage = imageView.image
-        let aCGImage = aUIImage?.cgImage
-        aCIImage = CIImage(cgImage: aCGImage!)
-        context = CIContext(options: nil);
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveImage))
+        let cgImage = imageView.image?.cgImage
+        ciImage = CIImage(cgImage: cgImage!)
+        context = CIContext(options: nil)
     }
     
     // MARK: - Helper Functions -
@@ -179,33 +206,114 @@ class FilterImageViewController: UIViewController {
         imageView.image = newUIImage
     }
     
+    func addTextToPhoto(text: NSString, inImage: UIImage, atPoint:CGPoint) -> UIImage {
+        let textColor = UIColor.white.cgColor
+        let textFont = UIFont.systemFont(ofSize: 14)
+        let textFontAttributes = [
+            NSAttributedString.Key.foregroundColor: textColor,
+            NSAttributedString.Key.font: textFont
+        ] as [NSAttributedString.Key : Any]
+        
+        // Create bitmap based graphics context
+        UIGraphicsBeginImageContextWithOptions(inImage.size, false, 0.0)
+
+        //Put the image into a rectangle as large as the original image.
+        inImage.draw(in: CGRect(x: 0, y: 0, width: inImage.size.width, height: inImage.size.height))
+        
+        // Our drawing bounds
+        let drawingBounds = CGRect(x: 0.0, y: 0.0, width: inImage.size.width, height: inImage.size.height)
+        
+        let textSize = text.size(withAttributes: [NSAttributedString.Key.font:textFont])
+        let textRect = CGRect(x: drawingBounds.size.width/2 - textSize.width/2, y: drawingBounds.size.height/2 - textSize.height/2,
+                              width: textSize.width, height: textSize.height)
+        
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        // Get the image from the graphics context
+        let newImag = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImag!
+
+    }
+    
     // MARK: - Actions -
     @objc func brightnessValueChanged(sender: UISlider) {
         let brightnessFilter: CIFilter!
         brightnessFilter = CIFilter(name: "CIColorControls")
-        brightnessFilter.setValue(aCIImage, forKey: "inputImage")
-        brightnessValueLabel.text = "\(brightnessSlider.value)"
+        brightnessFilter.setValue(ciImage, forKey: "inputImage")
+        brightnessValueLabel.text = String(format: "%.02f", brightnessSlider.value)
         sliderControls(filter: brightnessFilter, key: "inputBrightness", sender: sender)
     }
     
     @objc func contrastValueChanged(sender: UISlider) {
         let contrastFilter: CIFilter!
         contrastFilter = CIFilter(name: "CIColorControls")
-        contrastFilter.setValue(aCIImage, forKey: "inputImage")
-        contrastValueLabel.text = "\(contrastSlider.value)"
+        contrastFilter.setValue(ciImage, forKey: "inputImage")
+        contrastValueLabel.text = String(format: "%.02f", contrastSlider.value)
         sliderControls(filter: contrastFilter, key: "inputContrast", sender: sender)
     }
     
     @objc func saturationValueChanged(sender: UISlider) {
         let saturationFilter: CIFilter!
         saturationFilter = CIFilter(name: "CIColorControls")
-        saturationFilter.setValue(aCIImage, forKey: "inputImage")
-        saturationValueLabel.text = "\(saturationSlider.value)"
+        saturationFilter.setValue(ciImage, forKey: "inputImage")
+        saturationValueLabel.text = String(format: "%.02f", saturationSlider.value)
         sliderControls(filter: saturationFilter, key: "inputSaturation", sender: sender)
     }
     
     @objc func saveImage() {
-        
+        let image = imageView.image
+        networkManager.uploadImage(imageData: (image?.pngData())!)
+    }
+    
+    @objc func presetFilterAction() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Mono", style: .default, handler: { (_) in
+            self.imageView.image = self.imageView.image?.addFilterToImage(filter: .mono)
+        }))
+        alert.addAction(UIAlertAction(title: "Sepia", style: .default, handler: { (_) in
+            self.imageView.image = self.imageView.image?.addFilterToImage(filter: .sepia)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func imageTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "Enter Text\n\n\n\n\n", message: nil, preferredStyle: .alert)
+
+        let cancelAction = UIAlertAction.init(title: "Cancel", style: .default) { (action) in
+            alertController.view.removeObserver(self, forKeyPath: "bounds")
+        }
+        alertController.addAction(cancelAction)
+
+        let saveAction = UIAlertAction(title: "Submit", style: .default) { (action) in
+            let text = self.textView.text
+            self.addTextToPhoto(text: text! as NSString, inImage: self.imageView.image!, atPoint: CGPoint(x: 0, y: 0))
+            alertController.view.removeObserver(self, forKeyPath: "bounds")
+        }
+        alertController.addAction(saveAction)
+
+        alertController.view.addObserver(self, forKeyPath: "bounds", options: NSKeyValueObservingOptions.new, context: nil)
+        textView.backgroundColor = UIColor.lightGray
+        textView.textContainerInset = UIEdgeInsets.init(top: 8, left: 5, bottom: 8, right: 5)
+        alertController.view.addSubview(self.textView)
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "bounds"{
+            if let rect = (change?[NSKeyValueChangeKey.newKey] as? NSValue)?.cgRectValue {
+                let margin: CGFloat = 8
+                let xPos = rect.origin.x + margin
+                let yPos = rect.origin.y + 54
+                let width = rect.width - 2 * margin
+                let height: CGFloat = 90
+
+                textView.frame = CGRect.init(x: xPos, y: yPos, width: width, height: height)
+            }
+        }
     }
 }
 
@@ -219,6 +327,14 @@ extension FilterImageViewController {
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             imageView.heightAnchor.constraint(equalToConstant: view.frame.height / 2.5),
             imageView.widthAnchor.constraint(equalToConstant: view.frame.width)
+        ])
+        
+        filterButtons.addArrangedSubview(presetFilterButton)
+        view.addSubview(filterButtons)
+        NSLayoutConstraint.activate([
+            filterButtons.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: standardPadding),
+            filterButtons.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: standardPadding),
+            filterButtons.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -standardPadding)
         ])
         
         brightnessStack.addArrangedSubview(brightnessValueLabel)
--- EulerityTakeHome/Networking/NetworkManager.swift
+++ EulerityTakeHome/Networking/NetworkManager.swift
@@ -82,7 +82,166 @@ class NetworkManager {
         }
     }
     
+    func fetchUploadURL(completion: @escaping (Result<URL?, Error>) -> Void) {
+        let url = baseURL.appendingPathComponent("upload")
+        let request = URLRequest(url: url)
+        URLSession.shared.loadData(using: request) { [weak self] (data, response, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            guard let response = response else {
+                let error = NSError(domain: "\(#file).\(#function)",
+                                    code: 999,
+                                    userInfo: [NSLocalizedDescriptionKey: "invalid response"])
+                
+                print("invalid response in \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard response.statusCode < 400 else {
+                let error = NSError(domain: "\(#file).\(#function)",
+                                    code: 999,
+                                    userInfo: [NSLocalizedDescriptionKey: "invalid response: \(response.statusCode), \(String(data: data ?? Data(), encoding: .utf8))"])
+                
+                print("invalid response code in \(#function): \(response.statusCode)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "\(#file).\(#function)",
+                                    code: response.statusCode,
+                                    userInfo: [NSLocalizedDescriptionKey: "No Data Received: \(response.statusCode)"])
+                
+                print("no data received in \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let urlObject = try jsonDecoder.decode(Image.self, from: data)
+                completion(.success(urlObject.url))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
     // MARK: - Upload Methods -
+    func uploadImage(imageData: Data, completion: @escaping (Result<Void, Error>) -> Void = { _ in }) {
+        fetchUploadURL { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let url):
+                guard let url = url else {
+                    let error = NSError(domain: #function, code: 999, userInfo: [NSLocalizedDescriptionKey: "unable to get valid URL to upload file with"])
+                    completion(.failure(error))
+                    return
+                }
+                self.uploadImage(data: imageData, url: url, completion: completion)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func uploadImage(data: Data, url: URL, completion: @escaping (Result<Void, Error>) -> Void = { _ in }) {
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        let uuidString = UUID().uuidString
+        request.setValue("multipart/form-data; boundary=\(uuidString)", forHTTPHeaderField: "Content-Type")
+        
+        let body = NSMutableData()
+        let boundary = "Boundary-\(uuidString)"
+        // Default email
+        body.append(convertData(email: "toscleveland@gmail.com", url: url, mime: "image/jpeg", fileData: data, boundary: boundary))
+        
+        URLSession.shared.loadData(using: request) { (data, response, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            guard let response = response else {
+                let error = NSError(domain: "\(#file) \(#function)",
+                                    code: 999,
+                                    userInfo: [NSLocalizedDescriptionKey: "invalid response"])
+                print("Invalid Response \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard response.statusCode < 400 else {
+                let error = NSError(domain: "\(#file) \(#function)",
+                                    code: 999,
+                                    userInfo: [NSLocalizedDescriptionKey: "invalid response: \(response.statusCode), \(String(data: data ?? Data(), encoding: .utf8))"])
+                print("\(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "\(#file) \(#function)",
+                                    code: response.statusCode,
+                                    userInfo: [NSLocalizedDescriptionKey: "No Data: \(response.statusCode)"])
+                print("No Data: \(#function)")
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            print(data)
+        }
+    }
+    
+    func convertData(email: String, url: URL, mime: String, fileData: Data, boundary: String) -> Data {
+        let data = NSMutableData()
+        // TODO: clean this up using convertFormField?
+        data.append(string: "--\(boundary)\r\n")
+        data.append(string: "Content-Disposition: form-data; name=\"appid\"\r\n")
+        
+        data.append(string: email)
+        data.append(string: "--\(boundary)\r\n")
+        data.append(string: "Content-Disposition: form-data; name=\"original\"\r\n")
+        
+        data.append(string: url.absoluteString)
+        data.append(string: "--\(boundary)\r\n")
+        data.append(string: "Content-Disposition: form-data; name=\"file\"")
+        data.append(string: "Content-Type: \(mime)\r\n\r\n")
+        
+        data.append(fileData)
+        data.append(string:"\r\n")
+        data.append(string: "--\(boundary)-")
+        return data as Data
+    }
+    
+    private func convertFormField(name: String, value: String, using boundary: String) -> String {
+        let fieldString =
+        """
+        --\(boundary)
+        Content-Disposition: form-data; name=\"\(name)\"
+
+        \(value)
+        
+        """
+        return fieldString
+    }
     
     // MARK: - Storage Operations -
     func cacheImageData(_ data: Data, for url: URL) {
